3a656975fcae10a9adec205afb5dcc8adb03af0d
stemastr777 <87226060+stemastr777@users.noreply.github.com>
2022-08-03 21:37:31 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import openpyxl
 
 # Opening
 st.title('Batubara menyebabkan kenaikan Harga Listrik Sektor Rumah Tangga Tahun 2016-2021 di Jawa Tengah')
